c336e2683723477c6973da8897199985a7f18595
Mateo Manea <manea96m@gmail.com>
2024-05-15 01:56:44 -0300

    Primer commit

---
+++ src/components/CartWidget/index.jsx
@@ -0,0 +1,9 @@
+export const CartWidget = () => {
+    return (
+        <>
+            <i className="cart bi bi-bag"><span className='cart__count'>2</span></i>
+        </>
+    )
+}
+
+export default CartWidget;
---
+++ src/components/NavBar/index.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import CartWidget from '../CartWidget';
+
+export const NavBar = () => {
+    return (
+        <header className="header">
+            <nav className="nav">
+                <h1 className="nav__title" id="navTitle">Code. Craft</h1>
+                <ul className="nav__menu">   
+                    <li><a className="nav__link" href="index.html">Inicio</a></li>
+                    <li><a className="nav__link" href="#">Productos</a></li>
+                    <li><a className="nav__link" href="#">Contacto</a></li>
+                </ul>
+
+                <div className="nav__dashboard">
+                    <a className="nav__link--cart" href="#">
+                        <CartWidget/>
+                    </a>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar;
